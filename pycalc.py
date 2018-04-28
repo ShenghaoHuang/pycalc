46a891ea858c5ee368565de912658a3227021476
Alexey Karpenko <38530214+k-alexey@users.noreply.github.com>
2018-04-28 09:03:37 +0300

    Add func paramtheses

--- pycalc.py
+++ pycalc.py
@@ -7,16 +7,8 @@ import re
 import string
 import sys
 import operator
-
-def check_parentheses(t_expr):
-    parentheses = 0
-    for (_, v) in t_expr:
-        if v == '(': parentheses += 1
-        if v == ')': parentheses -= 1
-        if parentheses < 0: raise ValueError("Wrong parentheses")
-    else:
-        if parentheses > 0: raise ValueError("Wrong parentheses")
-
+from collections import deque
+from pprint import pprint
 
 parser = argparse.ArgumentParser("pycalc", description='Pure-python command-line calculator',
                                  usage='%(prog)s EXPRESSION [-h] [-v] [-m [MODULE [MODULE ...]]]')
@@ -28,9 +20,11 @@ verbose = args.verbose
 expr = str(args.EXPRESSION)
 expr = re.sub('[^{}]'.format(string.ascii_letters + string.digits + '+\-*/^%><=,.!_()'), '', expr)
 
+
 if verbose:
     print('ARGS:\t', vars(args))
     print('EXPR:\t', expr)
+
 
 for module in args.use_modules:
     try:
@@ -43,28 +37,28 @@ for module in args.use_modules:
 
 
 tokens = (
-    ('FLOAT', r'\d*\.\d+'),
-    ('INTEGER', r'\d+'),
-    ('LPARENT', r'\('),
-    ('RPARENT', r'\)'),
-    ('PLUS', r'\+'),
-    ('MINUS', r'-'),
-    ('TIMES', r'\*'),
-    ('DIVIDE', r'/'),
-    ('FUNC', r'[a-zA-Z_][a-zA-Z0-9_.]*'),
-    ('COMMA', r'\,'),
-    ('POWER', r'\^'),
-    ('FDIVIDE', r'//'),
-    ('MODULO', r'%'),
-    ('EQUALS', r'=='),
-    ('LE', r'<='),
-    ('LT', r'<'),
-    ('GE', r'>='),
-    ('GT', r'>'),
-    ('NE', r'!='),
+    ('FLOAT', re.compile(r'\d*\.\d+')),
+    ('INTEGER', re.compile(r'\d+')),
+    ('LPARENT', re.compile(r'\(')),
+    ('RPARENT', re.compile(r'\)')),
+    ('PLUS', re.compile(r'\+')),
+    ('MINUS', re.compile(r'-')),
+    ('TIMES', re.compile(r'\*')),
+    ('DIVIDE', re.compile(r'/')),
+    ('FUNC', re.compile(r'[a-zA-Z_][a-zA-Z0-9_.]*\(')),  # TODO : add func.() exception
+    ('CONST', re.compile(r'[a-zA-Z_][a-zA-Z0-9_.]*')),  # TODO : same
+    ('COMMA', re.compile(r'\,')),
+    ('POWER', re.compile(r'\^')),
+    ('FDIVIDE', re.compile(r'//')),
+    ('MODULO', re.compile(r'%')),
+    ('EQUALS', re.compile(r'==')),
+    ('LE', re.compile(r'<=')),
+    ('LT', re.compile(r'<')),
+    ('GE', re.compile(r'>=')),
+    ('GT', re.compile(r'>')),
+    ('NE', re.compile(r'!=')),
 )
-compiled_tokens = [(t, re.compile(t_re)) for (t, t_re) in tokens]
-runtokens = (
+token_ops = (
     ('FLOAT', float),
     ('INTEGER', int),
     ('PLUS', operator.add),
@@ -81,10 +75,23 @@ runtokens = (
     ('GT', operator.gt),
     ('NE', operator.ne),
 )
-token_expr = []
+# precedence = {
+# 'FLOAT','INTEGER',
+# 'LPARENT','RPARENT',
+# 'PLUS','MINUS',
+# 'TIMES','DIVIDE','FDIVIDE','MODULO',
+# 'POWER',
+#
+# 'FUNC',
+# 'CONST',
+# 'COMMA',
+# 'EQUALS','LE','LT',     'GE',     'GT',     'NE',
+# }
 
+
+token_expr = []
 while expr:
-    for (t, t_cre) in compiled_tokens:
+    for (t, t_cre) in tokens:
         t_match = t_cre.match(expr)
         if t_match:
             token_expr.append((t, t_match.group()))
@@ -93,10 +100,34 @@ while expr:
     else:
         raise ValueError("EXPRESSION Tokenization Error")
 
-if verbose:
-    print(*token_expr, sep='\n')
+for i, (t, v) in enumerate(token_expr):
+    if verbose:
+        print(f'{i:4} {t:10} {v}')
+    token_expr[i] = (i, t, v)
 
-check_parentheses(token_expr)
+parent_level = 0
+func_levels = []
+for (i, t, v) in token_expr:
+    if t == 'LPARENT':
+        parent_level += 1
+    if t == 'FUNC':
+        parent_level += 1
+        func_levels.append(parent_level)
+    if t == 'COMMA' and parent_level != func_levels[-1]:
+        raise ValueError("Comma error")
+    if t == 'RPARENT':
+        if func_levels and func_levels[-1] == parent_level:
+            token_expr[i] = (i, 'FRPARENT', v)
+            func_levels.pop()
+        parent_level -= 1
+        if parent_level < 0:
+            raise ValueError("Wrong parentheses")
+else:
+    if parent_level > 0: raise ValueError("Wrong parentheses")
+
+for i, t, v in token_expr:
+    if verbose:
+        print(f'{i:4} {t:8} {v:8}')
 
 result = 0
 
